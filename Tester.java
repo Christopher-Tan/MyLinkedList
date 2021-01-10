40d883d178f2cbb18a9bc93c31ce721c4e44f336
Christopher Tan <ctan20@stuy.edu>
2021-01-09 19:31:24 -0500

    Added a small test

--- Tester.java
+++ Tester.java
@@ -113,5 +113,23 @@ public class Tester {
         list1.extend(list2);
         System.out.println(list1);
         System.out.println(list2);
+        MyLinkedList a = new MyLinkedList();
+        MyLinkedList b = new MyLinkedList();
+        for(int i = 0; i < 10; i++){
+          if(i < 5){
+            a.add(i+"");
+          }else{
+            b.add(i+"");
+          }
+        }
+        System.out.println();
+        System.out.println("A:"+a+a.size());
+        System.out.println("B:"+b+b.size());
+    
+        a.extend(b);
+        System.out.println("A:"+a+a.size());
+        System.out.println("B:"+b+b.size());
+        System.out.println("A reversed:"+a.toStringReversed()+a.size());
+        System.out.println("B reversed:"+b.toStringReversed()+b.size());
     }
 }
